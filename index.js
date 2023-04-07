52a3e25034c76a463943fb3592d0a11b39d32458
Rupam Ghosh <117834750+impom@users.noreply.github.com>
2023-04-07 23:50:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+var cMode = "Light";
+function changeBg(){
+    if(cMode == "Dark") {
+    body.style.backgroundColor= "white";
+    body.style.color ="black";
+    mode.src = "Images/dark.png";
+    success.style.opacity = 1;
+    success.innerHTML = "Light Mode Applied";
+    var cMode = "Light"
+    setTimeout(timeout, 2000);
+    function timeout()
+    {
+        success.style.opacity ="0";
+    };
+    }
+    else {
+    var cMode = "Dark";
+    success.style.opacity = 1;
+    success.innerHTML = "Dark Mode Applied";
+    document.body.style.backgroundColor = "#121212";
+    document.body.style.color = "white";
+    document.getElementById("mode").src = "Images/light.png";
+    setTimeout(timeout, 2000);
+    function timeout()
+    {
+        success.style.opacity ="0";
+    }
+}
+}
